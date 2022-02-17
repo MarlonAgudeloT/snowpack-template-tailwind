f42bdb68ce3834d8b4ec1d70261f26b3213c37f8
MarlonAgudeloT <MarlonAgudeloT@users.noreply.github.com>
2022-02-17 10:45:42 +0000

    deploy: 58277423c8e3cf316d59e4913c67b93ae319efa9

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,6 +1,33 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+// Because img is a relative route
+// The url is necessary because the route is relative
+ const baseUrl ="https://platzi-avo.vercel.app"
+ //Renderingin an div with an id app into index.html
+ const appNode = document.querySelector('#app');
+ //web api
+ async function fetchData() {
+   const response = await fetch(`${baseUrl}/api/avo`),
+   data = await response.json(),
+   allItems = [];
+ 
+   data.data.forEach((item) => {
+     // create image
+     const image = document.createElement("img");
+     // Read the image from a relative route
+        image.src = `${baseUrl}${item.image}`;
+     // create title
+     const title = document.createElement("h2");
+        title.textContent = item.name;
+     // create price
+     const price = document.createElement("div");
+        price.textContent = item.price;
 
-console.log('Happy hacking :)')
+     const container = document.createElement("div");
+     container.append(image, title, price);
+ 
+     allItems.push(container);
+   });
+ 
+   appNode.append(...allItems)
+ }
+ 
+ fetchData();
